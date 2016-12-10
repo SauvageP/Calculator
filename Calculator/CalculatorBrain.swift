066bbe06c8ecae1585672adb499afe92d97b1389
Perry Gabriel <prgabriel88@gmail.com>
2016-12-10 17:32:18 -0500

    Added additional math operation. Cleaned up CalculatorBrain.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,10 +8,6 @@
 
 import Foundation
 
-func multiply(_ op1: Double,_ op2: Double) -> Double {
-	return op1 * op2
-}
-
 class CalculatorBrain {
 
 	private var accumulator = 0.0
@@ -20,16 +16,20 @@ class CalculatorBrain {
 		accumulator = operand
 	}
 	
-	var operations: Dictionary<String, Operation> = [
+	private var operations: Dictionary<String, Operation> = [
 		"π" : Operation.Constant(M_PI),
 		"e" : Operation.Constant(M_E),
 		"√" : Operation.UnaryOperation(sqrt),
 		"cos" : Operation.UnaryOperation(cos),
-		"x" : Operation.BinaryOpertaion(multiply),
+		"x" : Operation.BinaryOpertaion({$0 * $1}),
+		"-" : Operation.BinaryOpertaion({$0 - $1}),
+		"÷" : Operation.BinaryOpertaion({$0 / $1}),
+		"+" : Operation.BinaryOpertaion({$0 + $1}),
+		"±" : Operation.UnaryOperation({ -$0}),
 		"=" : Operation.Equals
 	]
 	
-	enum Operation {
+	private enum Operation {
 		case Constant(Double)
 		case UnaryOperation((Double) -> Double)
 		case BinaryOpertaion((Double, Double) -> Double)
@@ -49,12 +49,15 @@ class CalculatorBrain {
 	}
 	
 	private func executePendingBinaryOperation() {
+		if pending != nil {
 		accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+			pending = nil
+		}
 	}
 	
 	private var pending: PendingBinaryOpertaitonInfo?
 	
-	struct PendingBinaryOpertaitonInfo  {
+	private struct PendingBinaryOpertaitonInfo  {
 		var binaryFunction: (Double, Double) -> Double
 		var firstOperand: Double
 	}
